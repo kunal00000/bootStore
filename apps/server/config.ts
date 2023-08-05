d63de1d43b9319cac82e06667696575d9e0e1cf8
kunal00000 <kunalverma2468@gmail.com>
2023-08-05 20:16:40 +0530

    feat: adding mongo db uri

--- apps/server/config.ts
+++ apps/server/config.ts
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET as string;
+export const MONGODB_URI = process.env.MONGODB_URI as string;
