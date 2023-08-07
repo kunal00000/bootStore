69b42502864dc5a51a1c40c3b40f04aed8dd054b
kunal00000 <kunalverma2468@gmail.com>
2023-08-08 00:22:34 +0530

    feat: add stores to owners

--- apps/server/models/owner.models.ts
+++ apps/server/models/owner.models.ts
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const owner_db_schema = new mongoose.Schema({
   name: String,
   email: String,
-  password: String
+  password: String,
+  stores: [{ type: mongoose.SchemaTypes.ObjectId, ref: "store" }]
 });
 
 export const OwnerModel = mongoose.model("owner", owner_db_schema);
--- apps/server/types/owner.validation.ts
+++ apps/server/types/owner.validation.ts
@@ -5,7 +5,8 @@ export const owner_zod_schema = z
     _id: z.string().optional(),
     name: z.string().min(2).max(30),
     email: z.string().email(),
-    password: z.string().min(4).max(40)
+    password: z.string().min(4).max(40),
+    stores: z.array(z.string().uuid())
   })
   .strict();
 
