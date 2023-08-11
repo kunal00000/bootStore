66c8e3fba237cece0a81f2cf60bd9be3fb059cea
kunal00000 <kunalverma2468@gmail.com>
2023-08-11 23:53:40 +0530

    feat: add hero section

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import HeroCarousel from "../components/HeroCarousel";
 
 export default function Page() {
   return (
     <div className="w-full flex flex-col items-center h-[100vh] relative">
       <Navbar />
+      <HeroCarousel />
       <div>bkbjbkjbk</div>
     </div>
   );
